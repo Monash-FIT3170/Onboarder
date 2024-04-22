fd64f59d7e6a34b60bf7cee3365d5b75ac3fc800
Antony <araz0013@student.monash.edu>
2024-04-22 12:28:38 +1000

    Reformatted body into a json dumps

--- backend/lambda/app.py
+++ backend/lambda/app.py
@@ -1,14 +1,16 @@
+import json
 
 def lambda_handler(event, context):
+    body: {
+        'opening_id': 345678,
+        'recruitment_round_id': 45678,
+        'title': 'OpeningTitle',
+        'description': 'Description of Opening',
+        'app_role': 'Some text',
+        'status': 'Accepting'
+    }
     response = {
         'statusCode': 200,
-        'body': '''{
-            'opening_id': 345678,
-            'recruitment_round_id': 45678,
-            'title': 'OpeningTitle',
-            'description': 'Description of Opening',
-            'app_role': 'Some text',
-            'status': 'Accepting'
-        }'''
+        'body': json.dumps(body)
     }
     return response
