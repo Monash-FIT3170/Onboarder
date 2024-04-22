01540584a661712d7b875ce8fcfc24550574d039
Antony <araz0013@student.monash.edu>
2024-04-22 12:21:48 +1000

    Updated body of dummy lambda to an example opening record

--- backend/lambda/app.py
+++ backend/lambda/app.py
@@ -2,6 +2,13 @@
 def lambda_handler(event, context):
     response = {
         'statusCode': 200,
-        'body': 'DummyResponse'
+        'body': '''{
+            'opening_id': 345678,
+            'recruitment_round_id': 45678,
+            'title': 'OpeningTitle',
+            'description': 'Description of Opening',
+            'app_role': 'Some text',
+            'status': 'Accepting'
+        }'''
     }
     return response
