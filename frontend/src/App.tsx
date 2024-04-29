8e41eb8cc6fec2b1c42c8f9c29104beb47526bfc
Brendon perera <Bper0009@student.monash.edu>
2024-04-29 15:25:22 +1000

    trying to fix error in code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
+// @ts-ignore
 import React from "react";
 import { Link, Route, Routes } from "react-router-dom";
 
 import LoginPage from "../src/screens/LoginPage";
 import RegisterPage from "../src/screens/RegisterPage";
 import RecruitmentPage from "../src/screens/RecruitmentPage";
+import ViewOpenPage from "../src/screens/ViewOpeningPage";
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
         <Route path="/" element={<RecruitmentPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/viewopen" element={<ViewOpenPage />} />
       </Routes>
     </>
   );
