cdd89e845e293988f1101035cfbc3e9f6b379baf
araz0013 <araz0013@student.monash.edu>
2024-05-10 15:28:59 +1000

    Added routing for all required endpoints and dummy data is returned

--- backend/lambda/router.py
+++ backend/lambda/router.py
@@ -6,6 +6,7 @@ import controller
 
 routes = dict()
 
+### maybe just use fastapi
 
 def dispatch(event: dict):
     
@@ -16,8 +17,9 @@ def dispatch(event: dict):
     func = routes.get(resource, {}).get(method)
 
     path_params = event.get('pathParameters', {})
+    print(event)
     querystring_params = event.get('queryStringParameters', {})
-    body = event.get('body')
+    body = event.get('body', {})
     # possible validation here
 
     result = func(path_params, querystring_params, body)
@@ -41,19 +43,127 @@ def route(path: str, methods: list[str]) -> Callable:
     return inner
 
 
-@route('/openings', ['GET'])
-def openings(params: dict = {}, _ = {}, __ = {}, ___ = {}):
-    body = {
-        'opening_id': 345678,
-        'recruitment_round_id': 45678,
-        'title': 'OpeningTitle',
-        'description': 'Description of Opening',
-        'app_role': 'Some text',
-        'status': 'Accepting'
-    }
+### RECRUITMENT ROUNDS
+
+@route('/recruitmentRounds', ['GET'])
+@route('/recruitmentRounds/{roundId}', ['GET'])
+def fetch_recruitment_rounds(path_params = {}, _ = {}, __ = {}):
+    round_id = None
+    # parameter validation
+    if path_params:
+        round_id = path_params.get('roundId')
+    if round_id:
+        records = controller.get_specific_rec_round(round_id)
+    else:
+        records = controller.get_all_rec_rounds()
 
     response = {
         'statusCode': 200,
-        'body': json.dumps(body)
+        'body': json.dumps(records)
+    }
+    return response
+
+
+@route('/recruitmentRounds', ['POST'])
+def create_recruitment_round(_ = {}, __ = {}, ___ = {}):
+    controller.create_rec_round()
+    response = {
+        'statusCode': 201,
+    }
+    return response
+
+
+### OPENINGS 
+
+
+@route('/openings', ['GET'])
+def get_all_openings(_ = {}, __ = {}, ___ = {}):
+    
+    records = controller.get_all_openings()
+
+    response = {
+        'statusCode': 200,
+        'body': json.dumps(records)
+    }
+    return response
+
+@route('/recruitmentRounds/{roundId}/openings', ['GET'])
+@route('/recruitmentRounds/{roundId}/openings/{openingId}', ['GET'])
+def get_openings_for_round(path_params = {}, _ = {}, __ = {}):
+    # parameter validation
+    round_id = path_params.get('roundId')
+    opening_id = path_params.get('openingId')
+    if opening_id:
+        records = controller.get_specific_open_for_round(round_id, opening_id)
+    else:
+        records = controller.get_all_opens_for_round(round_id)
+
+    response = {
+        'statusCode': 200,
+        'body': json.dumps(records)
+    }
+    return response
+
+@route('/recruitmentRounds/{roundId}/openings', ['POST'])
+def create_opening(_ = {}, __ = {}, ___ = {}):
+    
+    controller.create_opening()
+
+    response = {
+        'statusCode': 201
+    }
+    return response
+
+
+### APPLICATIONS
+
+@route('/openings/{openingId}/applications', ['POST'])
+def create_application(_ = {}, __ = {}, ___ = {}):
+    
+    controller.create_application()
+
+    response = {
+        'statusCode': 201
+    }
+    return response
+
+
+@route('/openings/{openingId}/applications', ['GET'])
+def get_applications_for_opening(path_params = {}, _ = {}, __ = {}):
+    # parameter validation
+    opening_id = path_params.get('openingId')
+    records = controller.get_all_applications_for_opening(opening_id)
+
+    response = {
+        'statusCode': 200,
+        'body': json.dumps(records)
+    }
+    return response
+
+
+@route('/applications/{applicationId}', ['GET'])
+def get_application(path_params = {}, _ = {}, __ = {}):
+    # parameter validation
+    application_id = path_params.get('applicationId')
+    records = controller.get_application(application_id)
+
+    response = {
+        'statusCode': 200,
+        'body': json.dumps(records)
+    }
+    return response
+
+
+@route('/applications/{applicationId}/accept', ['POST'])
+@route('/applications/{applicationId}/reject', ['POST'])
+def create_application(_ = {}, __ = {}, ___ = {}):
+    
+    if True: # path = accept
+        controller.accept_application()
+    else:
+        controller.reject_application()
+
+    response = {
+        'statusCode': 201
     }
     return response
