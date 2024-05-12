f0f87b3469c9cf606b3490f8823bfcf7bf554635
umoh0005 <umoh0005@student.monash.edu>
2024-05-12 19:10:01 +1000

    Created basic frontend UI for Create Opening page.

--- frontend/src/screens/CreateOpeningPage.tsx
+++ frontend/src/screens/CreateOpeningPage.tsx
@@ -1,6 +1,54 @@
+import { Stack, Button, Typography, TextField } from '@mui/material'
+
 function CreateOpeningPage() {
 
-    return <div>Create User Openings</div>;
+    return (
+      <Stack spacing={6} alignItems="center">
+
+        <Stack>
+          <Typography variant='h3'>Create Opening</Typography>
+        </Stack>
+
+        <Stack direction='column' spacing={12.5}>
+
+          <Stack direction='row' spacing={20}>
+            <Stack direction='row' spacing={5}>
+              <Typography variant='body2' fontSize={30}>Name:</Typography>
+              <TextField label='Enter Opening Name' size='small'/>
+            </Stack>
+            <Stack direction='row' spacing={5}>
+              <Typography variant='body2' fontSize={30}>For Round:</Typography>
+              <TextField label='Enter Round Name' size='small'/>
+            </Stack>
+          </Stack>
+
+          <Stack direction='row' spacing={20}>
+            <Stack direction='row' spacing={5}>
+              <Typography variant='body2' fontSize={30}>Description:</Typography>
+              <TextField label='Enter Opening Description' size='small'/>
+            </Stack>
+            <Stack direction='row' spacing={5}>
+              <Typography variant='body2' fontSize={30}>Deadline:</Typography>
+              <TextField label='Enter Deadline' size='small'/>
+            </Stack>
+          </Stack>
+
+          <Stack direction='row' spacing={20}>
+            <Stack direction='row' spacing={5}>
+              <Typography variant='body2' fontSize={30}>Skills:</Typography>
+              <TextField label='Enter Skills' size='small'/>
+            </Stack>
+          </Stack>
+
+        </Stack>
+
+        <Stack direction='row' spacing={5} alignItems="center">
+            <Button variant='contained' color='primary'>Submit</Button>
+            <Button variant='contained' color='warning'>Cancel</Button>
+        </Stack>
+
+      </Stack>
+    )
   }
   
   export default CreateOpeningPage;
