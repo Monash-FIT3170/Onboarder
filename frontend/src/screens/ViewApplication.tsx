dc01c8b14bf86b0b3a50cf1f2e71f4a5b1834e9a
sai-ramishetty <sairamishetty9@outlook.com>
2024-05-19 12:32:26 +1000

    Update to viewapplication page
    [UPDATE] Ensure the code is using more formatted, used table and grid instead of boxes and made the components read-only

--- frontend/src/screens/ViewApplication.tsx
+++ frontend/src/screens/ViewApplication.tsx
@@ -1,94 +1,85 @@
 import * as React from 'react';
-import { Box, TextField, Button, Typography } from '@mui/material';
-import ArrowBackRoundedIcon from '@mui/icons-material/ArrowBackRounded';
+import { Grid, TextField, Button, Typography, Table, Paper, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+import axios from 'axios';
 
 export default function RecruitmentPlatform() {
   return (
-    <Box
-      component="form"
-      sx={{
-        '& .MuiTextField-root': { m: 1, width: '70ch'},
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <Typography variant="h4" component="div" sx={{ width: '100%', marginLeft:'-10px', marginTop:'-10px', backgroundColor: '#1f8ae7', color: '#fff', padding: '10px' }}>
+    <>
+      <Typography variant="h4" component="div" sx={{ width: '100%', marginLeft: '-10px', marginTop: '-10px', backgroundColor: '#1f8ae7', color: '#fff', padding: '10px' }}>
         Onboarding: Recruitment Platform
       </Typography>
-      <Typography variant="h5" component="div" marginTop="20px" marginBottom="20px" >
-      <ArrowBackRoundedIcon sx={{ verticalAlign: 'middle', marginRight: '8px', marginLeft: '10px', color:'#1f8ae7', cursor:'pointer'}} />
+      <Typography variant="h5" component="div" sx={{ width: '50%',marginTop:'30px' }}>
         Opening Name
       </Typography>
-      <Box sx={{display: 'flex', flexDirection: 'row', alignItems: 'center', marginBottom: '20px'}}>
-        <Typography variant='h6' component="div" sx={{ width: '50%', marginLeft: '10px' }} >
-          Recruitment Round:
-        </Typography>
-        <Typography variant="h6" component="div" sx={{ width: '50%', marginLeft: '10px' }} >
-          Applications Received:
-        </Typography>
-      </Box>
-      <hr style={{ width: '100%', color: '#1f8ae7', opacity: '0.5', marginBottom:'15px' }} />
-      <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', marginBottom:'40px' }}>
-        <Typography variant="body1" component="div" sx={{ width: '50%', marginLeft:'10px'}}>
-          Monash Nova Rover Recruitment
-        </Typography>
-        <Typography variant="body1" component="div" sx={{ width: '50%' }}>
-          2
-        </Typography>
-      </Box>
-      <hr style={{ width: '100%', color: '#1f8ae7', opacity: '0.5', marginTop: '-25px' }} />
-      <Box>
-        <Typography variant="h5" component="div" marginLeft="10px" marginBottom='20px'marginTop='20px'>
+      <TableContainer component={Paper} sx={{marginTop:'20px'}}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Student team</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              <TableCell>
+                Monash Nova Rover
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+      <Grid item xs={12}>
+        <Typography variant="h5" component="div" sx={{ marginLeft: '10px', marginBottom: '20px', marginTop: '20px' }}>
           Applicant Info
         </Typography>
-        <Box sx={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap'}}>
-          <Box sx={{ width: '50%', minWidth: '200px' }}>
-            <TextField required id="first-name" label="First Name" defaultValue="John" />
-          </Box>
-          <Box sx={{ width: '50%' }}>
-            <TextField required id="last-name" label="Last Name" defaultValue="Doe" />
-          </Box>
-          <Box sx={{ width: '50%' }}>
-            <TextField required id="email" label="Email" defaultValue="johndoe@email.com" />
-          </Box>
-          <Box sx={{ width: '50%' }}>
-            <TextField required id="phone-number" label="Phone Number" defaultValue="1234567890" />
-          </Box>
-          <Box sx={{ width: '50%' }}>
-            <TextField id="Additional-information" label="Additional Information" defaultValue="I am perfect for this role as ..." />
-          </Box>
-        </Box>
-      </Box>
-      <Box>
-        <Typography variant="h5" component="div"marginLeft="10px" marginBottom='20px' marginTop='20px'>
+      </Grid>
+      <Grid container item xs={12} spacing={2}>
+        <Grid item xs={6}>
+          <TextField required id="first-name" label="First Name" defaultValue="John" InputProps={{ readOnly: true }} fullWidth />
+        </Grid>
+        <Grid item xs={6}>
+          <TextField required id="last-name" label="Last Name" defaultValue="Doe" InputProps={{ readOnly: true }} fullWidth />
+        </Grid>
+        <Grid item xs={6}>
+          <TextField required id="email" label="Email" defaultValue="johndoe@email.com" InputProps={{ readOnly: true }} fullWidth />
+        </Grid>
+        <Grid item xs={6}>
+          <TextField required id="phone-number" label="Phone Number" defaultValue="1234567890" InputProps={{ readOnly: true }} fullWidth />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField id="Additional-information" label="Additional Information" defaultValue="I am perfect for this role as ..." InputProps={{ readOnly: true }} fullWidth />
+        </Grid>
+      </Grid>
+      <Grid item xs={12}>
+        <Typography variant="h5" component="div" sx={{ marginLeft: '10px', marginBottom: '20px', marginTop: '20px' }}>
           Course Info
         </Typography>
-        <Box sx={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap'}}>
-          <Box sx={{ width: '50%' }}>
-            <TextField required id="Course-name" label="Course Name" defaultValue="Bachelor of Engineering" />
-          </Box>
-          <Box sx={{ width: '50%' }}>
-            <TextField required id="Specialisation" label="Major" defaultValue="Mechanical Engineering" />
-          </Box>
-          <Box sx={{ width: '50%' }}>
-            <TextField required id="Skills" label="Skills" defaultValue="CAD, Programming" />
-          </Box>
-          <Box sx={{ width: '50%' }}>
-            <TextField required id="Semesters remaining" label="Semesters Remaining" defaultValue="2" />
-          </Box>
-        </Box>
-        <Box sx={{ display:'flex', minWidth:'full' }}>
-            <TextField fullWidth required id="Current semester" label="Current Remaining" defaultValue="2" />
-          </Box>
-      </Box>
-      <Box sx={{ display: 'flex', justifyContent: 'center', marginTop:'70px' }}>
+      </Grid>
+      <Grid container item xs={12} spacing={2}>
+        <Grid item xs={6}>
+          <TextField required id="Course-name" label="Course Name" defaultValue="Bachelor of Engineering" InputProps={{ readOnly: true }} fullWidth />
+        </Grid>
+        <Grid item xs={6}>
+          <TextField required id="Specialisation" label="Major" defaultValue="Mechanical Engineering" InputProps={{ readOnly: true }} fullWidth />
+        </Grid>
+        <Grid item xs={6}>
+          <TextField required id="Skills" label="Skills" defaultValue="CAD, Programming" InputProps={{ readOnly: true }} fullWidth />
+        </Grid>
+        <Grid item xs={6}>
+          <TextField required id="Semesters remaining" label="Semesters Remaining" defaultValue="2" InputProps={{ readOnly: true }} fullWidth />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField fullWidth required id="Current semester" label="Current Remaining" defaultValue="2" InputProps={{ readOnly: true }} />
+        </Grid>
+      </Grid>
+      <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'center', marginTop: '70px' }}>
         <Button variant="contained" sx={{ m: 1, backgroundColor: '#1f8ae7' }}>
           ACCEPT
         </Button>
         <Button variant="contained" color="error" sx={{ m: 1 }}>
           REJECT
         </Button>
-      </Box>
-    </Box>
+      </Grid>
+    </>
   );
 }
