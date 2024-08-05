2e55c63c49097d59342133a5ef16a25bd313e260
sai-ramishetty <sairamishetty9@outlook.com>
2024-08-05 16:07:05 +1000

    Create styleComponent
    Storing all the common elements in the pages together to avoid duplication of code

---
+++ frontend/src/components/styleComponent.tsx
@@ -0,0 +1,40 @@
+
+
+const styles = {
+  TitleWrapper : {
+    display: "flex",
+    alignitems: "center",
+    gap: "10px",
+  },
+
+  recruitmentRoundPage: {
+    fontFamily: "Arial, sans-serif",
+  },
+  section: {
+    marginBottom: "2rem",
+  },
+  addRoundButton: {
+    marginBottom: "1rem",
+  },
+  table: {
+    minWidth: 650,
+  },
+  tableHeader: {
+    backgroundColor: "#f2f2f2",
+  },
+  viewButton: {
+    backgroundColor: "#1976d2",
+    color: "white",
+    padding: "0.5rem 1rem",
+    border: "none",
+    borderRadius: "4px",
+    cursor: "pointer",
+  },
+  scrollableTableBody: {
+		height: "calc(100vh - 400px)",
+		overflowY: "auto",
+		display: "block",
+	}
+};
+
+export default styles;
