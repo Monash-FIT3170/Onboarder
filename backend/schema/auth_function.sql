a2102eec4e975c78957c8c96464bd5b5df9c0b78
Hanif Mohammad Asif <hmoh0035@student.monash.edu>
2024-08-07 14:44:39 +1000

    Refactor auth_function.sql to only allow emails from monash domain

--- backend/schema/auth_function.sql
+++ backend/schema/auth_function.sql
@@ -10,25 +10,10 @@ RETURNS TRIGGER
 LANGUAGE plpgsql 
 SECURITY DEFINER 
 AS $$
-DECLARE
-    email_exists BOOLEAN;
 BEGIN
     -- Check if the email domain is correct
     IF (NEW.email ILIKE '%@student.monash.edu') THEN
-        -- Check if the email already exists in the auth.users table
-        SELECT EXISTS (
-            SELECT 1 
-            FROM auth.users 
-            WHERE email = NEW.email
-        ) INTO email_exists;
-
-        IF email_exists THEN
-            -- If the email exists, allow the insertion
-            RETURN NEW;
-        ELSE
-            -- If the email does not exist, raise an exception
-            RAISE EXCEPTION 'No account exists with this email.';
-        END IF;
+         RETURN NEW;
     ELSE
         -- If the email domain is incorrect, raise an exception to prevent the insertion
         RAISE EXCEPTION 'Wrong email domain.';
