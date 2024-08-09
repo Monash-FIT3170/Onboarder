bc83c6df3ac4efa948e17ab09b488fe408ec33db
Hanif Mohammad Asif <hmoh0035@student.monash.edu>
2024-08-10 00:48:21 +1000

    Added user email display and signout button to navbar

--- frontend/src/components/AppBarOnboarder.tsx
+++ frontend/src/components/AppBarOnboarder.tsx
@@ -3,21 +3,36 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import CssBaseline from "@mui/material/CssBaseline";
+import Button from "@mui/material/Button";
+import { useAuthStore } from "../util/stores/authStore";
 
 function AppBarOnBoarder() {
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <CssBaseline>
-        <AppBar position="static" sx={{ margin: 0 }}>
-          <Toolbar>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Onboarding: Recruitment Platform
-            </Typography>
-          </Toolbar>
-        </AppBar>
-      </CssBaseline>
-    </Box>
-  );
+	const { user, signOut } = useAuthStore();
+
+	return (
+		<Box sx={{ flexGrow: 1 }}>
+			<CssBaseline>
+				<AppBar position="static" sx={{ margin: 0 }}>
+					<Toolbar>
+						<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+							Onboarding: Recruitment Platform
+						</Typography>
+
+						{user && (
+							<>
+								<Typography variant="body1" sx={{ marginRight: 2 }}>
+									{user.email}
+								</Typography>
+								<Button color="inherit" onClick={signOut}>
+									Sign out
+								</Button>
+							</>
+						)}
+					</Toolbar>
+				</AppBar>
+			</CssBaseline>
+		</Box>
+	);
 }
 
 export default AppBarOnBoarder;
