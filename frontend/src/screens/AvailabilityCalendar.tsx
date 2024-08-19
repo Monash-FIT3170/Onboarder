ae9f0419de995125cf0d4b7cc63fc14b489382a1
Rahul Sahni <rsah0008@student.monash.edu>
2024-08-19 11:01:25 +1000

    Imported all the packages required for the calender

---
+++ frontend/src/screens/AvailabilityCalendar.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
+import format from 'date-fns/format';
+import parse from 'date-fns/parse';
+import startOfWeek from 'date-fns/startOfWeek';
+import getDay from 'date-fns/getDay';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { enUS } from 'date-fns/locale';
+
