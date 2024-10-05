1a87160d4ceb7c2ed62951528e40a4214d9b4978
Jesse Cruickshank <jcru0005@student.monash.edu>
2024-10-06 00:47:01 +1000

    feat: Added meeting_link to database, and connected the UI

--- frontend/src/components/DashboardTable.tsx
+++ frontend/src/components/DashboardTable.tsx
@@ -23,6 +23,7 @@ export interface StudentTeamResultProps {
   student_team_name: string;
   user_team_role: string;
   student_team_owner: string;
+  student_team_meeting_link: string;
 }
 
 export interface DashboardTableProps {
@@ -51,8 +52,13 @@ const generateRowFunction = (
   const [leaveModalOpen, setLeaveModalOpen] = useState(false);
   const BASE_API_URL = getBaseAPIURL();
 
-  const handleView = (t_id: number, t_name: string, user_role: string) => {
-    authStore.updateTeamAndRole(t_id, t_name, user_role);
+  const handleView = (
+    t_id: number,
+    t_link: string,
+    t_name: string,
+    user_role: string,
+  ) => {
+    authStore.updateTeamAndRole(t_id, t_link, t_name, user_role);
     navigate("/viewrecruitmentround");
   };
 
@@ -125,6 +131,7 @@ const generateRowFunction = (
               onClick={() =>
                 handleView(
                   result.student_team_id,
+                  result.student_team_meeting_link,
                   result.student_team_name,
                   result.user_team_role,
                 )
